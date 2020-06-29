13e466cfd96f49bd5e38f049fc36d71817bef2cc
Idalis Paz <idalispaz@gmail.com>
2020-06-29 22:18:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,8 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-
-puts programmer_hash[:dennis_ritchie][:languages]
+ programmer_hash[:dennis_ritchie][:languages]
+ 
 end
 
 def adding_matz
